980cc32f8097fb5ccae2915e702838505806f753
connect2ady <124473916+connect2ady@users.noreply.github.com>
2023-02-05 00:45:51 +0000

    Update streamlit_app.py
    Added only selected fruits as well as all fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple', 'Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+# Display the selected table on the page.
+streamlit.dataframe(fruits_to_show)
+
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
